810ba649d97a878d1ceec8fcf99b70f7c773c214
Lenard <lpascua.r2m@gmail.com>
2025-10-26 22:09:09 +0800

    feat: Implement Allure Report

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 
-
-
+test('Go to google', async ({ page }) => {
+    await page.goto('https://www.google.com/');
+    console.log('[INFO] Go to Google');
+})
