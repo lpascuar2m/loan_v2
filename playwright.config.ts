ae18bdda16a011a8d4c045fcfc532a9d23fd76e8
Lenard <lpascua.r2m@gmail.com>
2025-10-29 15:57:24 +0800

    chore: configure dotenv in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,9 +7,9 @@ import * as os from 'node:os';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
